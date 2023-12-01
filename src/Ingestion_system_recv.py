8c1957fdaccd216a98664d3dbc36792054503ceb
Fabio <fabizen99@gmail.com>
2023-12-01 16:20:15 +0100

    reformat files according to pylint

--- src/Ingestion_system_recv.py
+++ src/Ingestion_system_recv.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/system/<path:route>', methods=['POST'])
 def receive_message(route):
     data = request.get_json()
@@ -12,6 +13,7 @@ def receive_message(route):
         return {'status': 'success', 'message': 'Message received'}
     else:
         return {'status': 'error', 'message': 'Invalid request'}
+
 
 if __name__ == '__main__':
     app.run(host='192.168.143.248', port=5000, debug=True)
