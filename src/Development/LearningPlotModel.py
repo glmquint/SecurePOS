0ac5eecee60aa209d451d0575a1b34c74300eeaa
Giacomo Volpi <109603384+giacomovolpi@users.noreply.github.com>
2024-03-01 18:14:29 +0100

    Unfinished work on model

--- src/Development/LearningPlotModel.py
+++ src/Development/LearningPlotModel.py
@@ -7,6 +7,13 @@ class LearningPlotModel:
     number_of_generations: int = None
     loss_threshold: float = None
 
+    def __init__(self, loss: float, number_of_generations: int, loss_threshold: float):
+        self.loss = loss
+        self.number_of_generations = number_of_generations
+        self.loss_threshold = loss_threshold
+
     def populate(self, storage_controller: StorageController):
         list = storage_controller.retrieveAll()
-        print(list)
+        self.loss = list[0][0]
+        self.number_of_generations = list[0][1]
+        self.loss_threshold = list[0][2]
--- src/Test/test_LearningPlotModel.py
+++ src/Test/test_LearningPlotModel.py
@@ -6,10 +6,13 @@ from src.Storage.dbConfig import DBConfig
 
 class TestLearningPlotModel(TestCase):
     dbConfig = DBConfig('training', 'learning_plot', ['loss', 'number_of_generations', 'loss_threshold'])
-
-    storageController = StorageController(dbConfig, type(LearningPlotModel()))
+    lpm = LearningPlotModel(8.4, 100, 0.01)
+    lpm2 = LearningPlotModel(0, 0, 0)
+    storageController = StorageController(dbConfig, type(lpm))
     def test_populate(self):
-        pass
+        self.storageController.save(self.lpm)
+        self.lpm2.populate(self.storageController)
+        print(self.lpm2.loss)
 
 
 
