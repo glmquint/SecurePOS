9366fc64f8eafe5b9b4ff756901c3d218e4fb048
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-02 17:00:42 +0100

    modify models

--- src/Development/TestReportModel.py
+++ src/Development/TestReportModel.py
@@ -1,11 +1,9 @@
 from src.Development.TestRow import TestRow
-from src.Storage.StorageController import StorageController
 
 
 class TestReportView:
-    rows : [TestRow] = None
+    rows: [TestRow] = None
 
-    def __init__(self, storage_controller: StorageController):
-        rows = storage_controller.retrieve_all()
-        for row in rows:
+    def __init__(self, from_tuples: [tuple]):
+        for row in from_tuples:
             self.rows.append(TestRow(row[0], row[1], row[2], row[3]))
--- src/Development/ValidationReportModel.py
+++ src/Development/ValidationReportModel.py
@@ -1,12 +1,10 @@
 from src.Development.ValidationRow import ValidationRow
-from src.Storage.StorageController import StorageController
 
 
 class ValidationReportView:
     validation_rows: [ValidationRow] = None
 
-    def __init__(self, storage_controller: StorageController):
-        rows = storage_controller.retrieve_all()
-        for row in rows:
+    def __init__(self, from_tuples: [tuple]):
+        for row in from_tuples:
             self.validation_rows.append(ValidationRow(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]))
 
