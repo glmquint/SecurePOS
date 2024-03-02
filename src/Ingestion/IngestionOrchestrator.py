d242f2788573e1228dafde7980ea2615021714af
Guillaume Quint <glmquint@gmail.com>
2024-03-02 19:07:32 +0100

    ingestion orchestrator skeleton

--- src/Ingestion/IngestionOrchestrator.py
+++ src/Ingestion/IngestionOrchestrator.py
@@ -1,3 +1,5 @@
+from threading import Thread
+
 from src.Ingestion.PhaseTracker import PhaseTracker
 from src.Ingestion.PreparationSysReceiver import PreparationSysReceiver
 from src.Ingestion.PreparationSystemConfig import PreparationSystemConfig
@@ -6,20 +8,17 @@ from src.Ingestion.RawSessionCreator import RawSessionCreator
 from src.MessageBus.MessageBus import MessageBus
 from src.Storage.StorageController import StorageController
 
-class PreparationSystemOrchestrator
+class PreparationSystemOrchestrator:
     def __init__(self):
         self.config = PreparationSystemConfig()
-        self.raw_session_creator = RawSessionCreator()
-        self.prepared_session_creator = PreparedSessionCreator()
+        self.storage_controller = StorageController(dbConfig={"name": "raw_session", "columns": ["device_src", "samples"], "tableName":"raw_session"}, type=type(RawSession))
+        self.message_bus = MessageBus(topics=["PreparedSession"])
         self.phase_tracker = PhaseTracker()
-        self.storage_controller = StorageController()
-        self.preparation_sys_receiver = PreparationSysReceiver()
-        self.message_bus = MessageBus()
+        self.raw_session_creator = RawSessionCreator(self.storage_controller, self.phase_tracker)
+        self.prepared_session_creator = PreparedSessionCreator(self.message_bus, self.phase_tracker)
+        self.preparation_sys_receiver = PreparationSysReceiver(self.message_bus)
 
     def run(self):
-        self.prepared_session_creator.run()
-        self.phase_tracker.run()
-        self.storage_controller.run()
-        self.preparation_sys_receiver.run()
-        self.message_bus.run()
-        self.raw_session_creator.run()
+        Thread(target=self.raw_session_creator.run).start()
+        Thread(target=self.prepared_session_creator.run).start()
+        Thread(target=self.preparation_sys_receiver.run).start()
