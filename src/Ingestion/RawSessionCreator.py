8f002558f49c91f7430b81d04ff21c6b1ac50235
Guillaume Quint <glmquint@gmail.com>
2024-03-02 19:08:31 +0100

    raw session creator

--- src/Ingestion/RawSessionCreator.py
+++ src/Ingestion/RawSessionCreator.py
@@ -1,3 +1,40 @@
+from src.DataObjects.Record import Record
+from src.JsonIO.JSONSender import JSONSender
+
+DATAOBJ_PATH = "/src/DataObjects/Schema"
+
 class RawSessionCreator:
-    pass
+
+    def __init__(self, storage_controller, phase_tracker):
+        self.sufficient_number_of_records = 100
+        self.label_sender = JSONSender(f"{DATAOBJ_PATH}/labels.json", "http://127.0.0.1:8000/label")
+        self.raw_session_sender = JSONSender(f"{DATAOBJ_PATH}/raw_session.json", "http://127.0.0.1:8000/raw_session")
+        self.storage_controller = storage_controller
+        self.phase_tracker = phase_tracker
+
+    def retrieveRecords(self) -> [Record]:
+        pass
+
+    def isNumberOfRecordsSufficient(self) -> bool:
+        return self.storage_controller.count() > self.sufficient_number_of_records
+
+    def createRawSession(self, records:[Record]) -> None:
+        pass
+
+    def markMissingSamples(self, raw_session:RawSession) -> None:
+        pass
+
+    def isRawSessionValid(self, raw_session:RawSession) -> bool:
+        pass
+
+    def run(self) -> None:
+        while True:
+            if self.isNumberOfRecordsSufficient():
+                self.createRawSession()
+                self.storage_controller.remove()
+                self.markMissingSamples()
+                if self.isRawSessionValid():
+                    if self.phase_tracker.isEvalPhase():
+                        self.label_sender.send(self.label)
+                    self.raw_session_sender.send(self.raw_session)
 
