53cd1a4e91174cc91103fddb69a874273b818675
Fabio <fabizen99@gmail.com>
2024-03-02 18:41:40 +0100

    Simplify JsonValidator

--- src/JsonIO/JsonValidator.py
+++ src/JsonIO/JsonValidator.py
@@ -1,20 +1,15 @@
 import json
+
+import jsonschema
 from jsonschema import validate, ValidationError, Draft7Validator
 
 class JSONValidator:
     def __init__(self, schema_file):
         with open(schema_file, 'r') as f:
             self.schema = json.load(f)
-
-    def validate_json_string(self, json_string):
-        data = json.loads(json_string)
-        self.validate_data(data)
-
     def validate_data(self, data):
-        validator = Draft7Validator(self.schema)
-        errors = list(validator.iter_errors(data))
-        if errors:
-            error_messages = "\n".join(errors)
-            raise ValidationError(error_messages)
-        else:
-            print("Validation successful")
+        try:
+            jsonschema.validate(instance=data, schema=self.schema)
+            print("Validation successful.")
+        except jsonschema.exceptions.ValidationError as e:
+            raise Exception(e)
