9051c43b3e18493eb1aabbef7a5b96df9ab9c6b2
Guillaume Quint <glmquint@gmail.com>
2024-03-02 19:07:54 +0100

    preparation system receiver

--- src/Ingestion/PreparationSysReceiver.py
+++ src/Ingestion/PreparationSysReceiver.py
@@ -1,2 +1,17 @@
+from src.JsonIO.JSONEndpoint import JSONEndpoint
+from src.JsonIO.Server import Server
+
+DATAOBJ_PATH = "/src/DataObjects/Schema"
+
 class PreparationSysReceiver:
-    pass
+    def __init__(self, message_bus):
+        self.message_bus = message_bus
+        self.server = Server()
+        self.server.add_resource(JSONEndpoint, "/record", recv_callback=self.receiveRecord, json_schema_path=f"{DATAOBJ_PATH}/AttackRiskLabelSchema.json")
+        self.server.add_resource(JSONEndpoint, "/test_endpoint", recv_callback=self.receiveRawSession, json_schema_path=f"{DATAOBJ_PATH}/AttackRiskLabelSchema.json")
+
+    def receiveRecord(self, json_data):
+        pass
+
+    def receiveRawSession(self, json_data):
+        pass
