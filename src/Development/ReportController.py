4588b90926d914f53d302008d94df28ba25047e1
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-02 17:44:31 +0100

    No more needs for table_columns in dbConfig

--- src/Development/ReportController.py
+++ src/Development/ReportController.py
@@ -1,5 +1,5 @@
-from src.Development.LearningPlotModel import LearningPlotModel
-from src.Development.LearningPlotView import LearningPlotView
+from src.Development.Training.LearningPlotModel import LearningPlotModel
+from src.Development.Training.LearningPlotView import LearningPlotView
 from src.Storage.StorageController import StorageController
 from src.Storage.dbConfig import DBConfig
 
@@ -13,7 +13,7 @@ class ReportController:
         pass
 
     def create_learning_plot(self):
-        db_conf = DBConfig('training', 'learning_plot', ['loss', 'number_of_generations', 'loss_threshold'])
+        db_conf = DBConfig('training', 'learning_plot')
         storage_controller = StorageController(db_conf, type(LearningPlotModel((0, 0, 0))))
         learning_plot_models = storage_controller.retrieve_all()
         print(learning_plot_models)
