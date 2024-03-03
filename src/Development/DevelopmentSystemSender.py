0d7077007da63665bde4a7ec483a0497e0c4bb80
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-03 15:56:38 +0100

    Finish sender and receiver

--- src/Development/DevelopmentSystemSender.py
+++ src/Development/DevelopmentSystemSender.py
@@ -5,12 +5,12 @@ class DevelopmentSystemSender:
     messaging_sender: JSONSender = None
     production_sender: JSONSender = None
 
-    def __init__(self):
-        self.messaging_sender = JSONSender()
-        self.production_sender = JSONSender()
+    def __init__(self, url_messaging: str, url_production: str):
+        self.messaging_sender = JSONSender('../../json_schema/config_schema.json', url_messaging)
+        self.production_sender = JSONSender('../../json_schema/config_schema.json', url_production)
 
     def send_to_messaging(self, Config):
-        self.messaging_sender.send(Config)
+        self.messaging_sender.send(Config.toJSON())
 
     def send_to_production(self, Classifier):
-        self.production_sender.send(Classifier)
+        self.production_sender.send(Classifier.toJSON())
--- src/Development/LearningSetReceiver.py
+++ src/Development/LearningSetReceiver.py
@@ -1,15 +1,25 @@
+from src.JsonIO.Server import Server
 from src.MessageBus.MessageBus import MessageBus
 from src.JsonIO.JSONEndpoint import JSONEndpoint
+
 
 class LearningSetReceiver:
     learning_set = None
     message_bus_ref = None
-    def __init__(self, message_bus:MessageBus):
-        self.message_bus_ref = message_bus
+    server: Server = None
 
-    def receiveLearningSet(self):
-        # TODO: Implement the recv
-        learning_set = JSONEndpoint()
-        self.learning_set = learning_set
+    def __init__(self, message_bus: MessageBus):
+        self.message_bus_ref = message_bus
+        self.server = Server()
+        self.server.add_resource(JSONEndpoint, "/learning_set",
+                                 recv_callback=self.receive_learning_set,
+                                 json_schema_path="../../json_schema/learning_set_schema.json")
+
+    def receive_learning_set(self,json_data):
+        self.learning_set = LearningSet(**json_data)
         self.message_bus_ref.addTopic("LearningSet")
-        self.message_bus_ref.pushTopic("LearningSet", learning_set)
+        self.message_bus_ref.pushTopic("LearningSet", self.learning_set)
+        return 200
+
+    def run(self):
+        self.server.run()
