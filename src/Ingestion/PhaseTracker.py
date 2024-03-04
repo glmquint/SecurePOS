093a22eefe5609a78fcc5f3ba2b986e8d57fded3
Guillaume Quint <glmquint@gmail.com>
2024-03-04 19:28:29 +0100

    configurable phase tracker logic

--- src/Ingestion/PhaseTracker.py
+++ src/Ingestion/PhaseTracker.py
@@ -1,4 +1,12 @@
 class PhaseTracker:
     def __init__(self, config):
         self.config = config
+        self.count = 0
     pass
+
+    def increment(self):
+        self.count += 1
+        self.count %= self.config['production_phase_duration'] + self.config['evaluation_phase_duration']
+
+    def isEvalPhase(self):
+        return self.count > self.config['production_phase_duration']
