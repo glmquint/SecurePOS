1afb4555278a905432235430e2367b30759ca7f5
Guillaume Quint <glmquint@gmail.com>
2024-03-04 19:29:01 +0100

    sensible record init and missing sample check

--- src/DataObjects/Record.py
+++ src/DataObjects/Record.py
@@ -1,12 +1,12 @@
-from src.DataObjects.Sample import Sample
-
-
 class Record:
-    def __init__(self):
-        self.device_src: str = ""
-        self.timestamp: str = ""
-        self.amount: int = 0
-        self.target_ip: str = ""
-        self.dest_ip: str = ""
-        self.location_latitude: float = 0.0
-        self.location_longitude: float = 0.0
+    def __init__(self, device_src:str="", timestamp:str="", amount:int=0, target_ip:str="", dest_ip:str="", location_latitude:float=0.0, location_longitude:float=0.0) -> None:
+        self.device_src:str = device_src
+        self.timestamp:str = timestamp
+        self.amount:int = amount
+        self.target_ip:str = target_ip
+        self.dest_ip:str = dest_ip
+        self.location_latitude:float = location_latitude
+        self.location_longitude:float = location_longitude
+
+    def isMissingSample(self) -> bool:
+        return (self.device_src == "" or self.timestamp == "" or self.amount == 0 or self.target_ip == "" or self.dest_ip == "" or self.location_latitude == 0.0 or self.location_longitude == 0.0)
