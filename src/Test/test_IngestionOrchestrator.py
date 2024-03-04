8a0aa4a1a159fc73283bb2c8fd8d6403e70b264e
Guillaume Quint <glmquint@gmail.com>
2024-03-04 17:42:12 +0100

    running (not working) test for ingestion orchestrator

--- src/Test/test_IngestionOrchestrator.py
+++ src/Test/test_IngestionOrchestrator.py
@@ -1,6 +1,20 @@
+from threading import Thread
+from time import sleep
+
+import requests
+
+from src.DataObjects.Record import Record
+from src.Ingestion.PreparationSystemConfig import PreparationSystemConfig
 from src.Ingestion.IngestionOrchestrator import PreparationSystemOrchestrator
 
+DATAOBJ_PATH = "../DataObjects/Schema"
 
-def test_run():
-    config = PreparationSystemCofig()
-    IngesionOrchestrator = PreparationSystemOrchestrator()
+class TestPreparationSystemOrchestrator:
+    def test_run(self):
+        config = PreparationSystemConfig(f"{DATAOBJ_PATH}/PreparationSystemConfigSchema.json")
+        config.init_from_file("PreparationSystemConfig.json")
+        orchestrator = PreparationSystemOrchestrator(config)
+        Thread(target=orchestrator.run, daemon=True).start()
+        record = Record()
+        r = requests.post("http://127.0.0.1:5000/record", json=record.__dict__)
+        assert r.status_code == 200
