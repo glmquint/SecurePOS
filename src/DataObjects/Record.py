9c2719784fe2fcabbe07138dfa4d9bf8004bcdf5
Guillaume Quint <glmquint@gmail.com>
2024-03-04 17:37:47 +0100

    record class with schema

--- src/DataObjects/Record.py
+++ src/DataObjects/Record.py
@@ -2,5 +2,11 @@ from src.DataObjects.Sample import Sample
 
 
 class Record:
-    device_src = ""
-    samples = [Sample]
+    def __init__(self):
+        self.device_src: str = ""
+        self.timestamp: str = ""
+        self.amount: int = 0
+        self.target_ip: str = ""
+        self.dest_ip: str = ""
+        self.location_latitude: float = 0.0
+        self.location_longitude: float = 0.0
