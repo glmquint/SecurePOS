cae787f1dd1ad4a2f370b23aecfe42dd704b63dc
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-08 18:13:09 +0100

    Fix wrong check in start and finish set_avg_hyperparams

--- src/Development/Training/TrainProcess.py
+++ src/Development/Training/TrainProcess.py
@@ -3,7 +3,7 @@ import json
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
 from src.Development.Classifier import Classifier
-from src.Development.DevSystemStatus import DevSystemStatus
+from src.Development.DevelopmentSystemStatus import DevelopmentSystemStatus
 from src.Development.Training.HyperParameterLimit import HyperParameterLimit
 from src.MessageBus.MessageBus import MessageBus
 from src.Storage.StorageController import StorageController
@@ -19,14 +19,15 @@ class TrainProcess:
     grid_search: GridSearchCV = None
     hyperparameters: HyperParameterLimit = None
     avg_hyperparameters: dict = None
-    status: DevSystemStatus = None
+    status: DevelopmentSystemStatus = None
     learning_set: LearningSet = None
 
     def set_average_hyperparameters(self):
         self.avg_hyperparameters = {}
-        for key in self.hyperparameters.dict_hyperparameters.keys():
-            self.avg_hyperparameters[key] = (self.hyperparameters.dict_hyperparameters[key][0] +
-                                             self.hyperparameters.dict_hyperparameters[key][1]) / 2
+        for key in self.hyperparameters.__dict__:
+            print(self.hyperparameters.__dict__[key])
+            self.avg_hyperparameters[key] = (self.hyperparameters.__dict__[key]['min'] +
+                                             self.hyperparameters.__dict__[key]['max']) / 2
 
     def get_number_of_iterations(self) -> int:
         with open('number_of_iterations.json', 'r') as json_file:
@@ -34,19 +35,20 @@ class TrainProcess:
             return data['number_of_iterations']
         return -1
 
-    def __init__(self, status: DevSystemStatus, message_bus: MessageBus):
+    def __init__(self, status: DevelopmentSystemStatus, message_bus: MessageBus, hyperparameters: HyperParameterLimit):
         self.status = status
         self.message_bus = message_bus
+        self.hyperparameters = hyperparameters
 
     def train(self):
         pass
 
     def start(self):
         while True:
-            if self.status == "receive_learning_set":
+            if self.status.status == "receive_learning_set":
                 self.learning_set = self.message_bus.popTopic("LearningSet")
-                self.status.status = "set_avg_hyperparam"
-            elif self.status == "set_avg_hyperparam":
+                self.status.status = "set_avg_hyperparams"
+            elif self.status.status == "set_avg_hyperparams":
                 self.set_average_hyperparameters()
                 self.status.status = "set_number_of_iterations"
             elif self.status.status == "set_number_of_iterations":
@@ -57,7 +59,7 @@ class TrainProcess:
                     print("Error: number of iterations is not valid")
                     self.status.save_status()
                     break
-            elif self.status == "train":
+            elif self.status.status == "train":
                 self.train()
                 self.status.status = "check_validation"
                 break
