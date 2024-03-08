ec12df45ac5b711830cb231ef4921876ff02e840
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-08 18:13:39 +0100

    Change type of variables

--- src/Development/Training/LearningPlotModel.py
+++ src/Development/Training/LearningPlotModel.py
@@ -1,12 +1,9 @@
-
 class LearningPlotModel:
-    loss: [float] = None
+    loss_curve = None
     number_of_generations: int = None
     loss_threshold: float = None
 
-    def __init__(self, from_tuple: tuple):
-        self.loss, self.number_of_generations, self.loss_threshold = from_tuple
-
-    ([0.01,2e34,0.1],100,0.1)
-    0.01,100,0.1
-    2e43,100,0.1
+    def __init__(self, loss_curve: list, number_of_generations: int, loss_threshold: float):
+        self.loss_curve = loss_curve
+        self.number_of_generations = number_of_generations
+        self.loss_threshold = loss_threshold
