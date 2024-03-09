e8145cab3f5eebcd08c45a6382766130a5c35c65
Fabio <fabizen99@gmail.com>
2024-03-09 17:06:13 +0100

    refactor Content.py

--- src/DataObjects/Content.py
+++ src/DataObjects/Content.py
@@ -1,14 +1,14 @@
 # this is the object that is going to be sent to the ingestion system
 class Content:
-    def __init__(self, content, obj):
-        self.content = content
+    def __init__(self, class_type, obj):
+        self.class_type = class_type
         self.obj = obj
 
     def to_json(self):
         return {
-            "content": self.content,
+            "class_type": self.class_type,
             "obj": self.obj.to_json()
         }
 
     def __str__(self):
-        return f"content: {self.content}, obj: {self.obj.to_json()}"
+        return f"class_type: {self.class_type}, obj: {self.obj.to_json()}"
