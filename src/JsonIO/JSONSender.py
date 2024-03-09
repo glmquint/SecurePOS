c06a8954a31efd1508c910b1060190356dd4ee16
Fabio <fabizen99@gmail.com>
2024-03-09 17:06:04 +0100

    add case for no validation in sending

--- src/JsonIO/JSONSender.py
+++ src/JsonIO/JSONSender.py
@@ -7,11 +7,13 @@ class JSONSender:
     def __init__(self, json_schema_path : str, url : str):
         self.json_schema_path = json_schema_path
         self.url = url
-        self.validator = JSONValidator(json_schema_path)
+        if json_schema_path != "":
+            self.validator = JSONValidator(json_schema_path)
 
     def send(self, obj):
         try:
-            self.validator.validate_data(obj)
+            if self.json_schema_path != "":
+                self.validator.validate_data(obj)
             requests.post(self.url, json=obj)
         except Exception as e:
             print(e)
