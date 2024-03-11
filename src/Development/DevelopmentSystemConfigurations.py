0f744add7299fe5684901eef59fc34b4c18b1165
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-11 15:25:25 +0100

    Include loss threshold

--- src/Development/DevelopmentSystemConfigurations.py
+++ src/Development/DevelopmentSystemConfigurations.py
@@ -9,6 +9,7 @@ class DevelopmentSystemConfigurations:
     messaging_system_receiver: str = None
     hyperparameters: HyperParameterLimit = None
     validator: JSONValidator = None
+    loss_threshold: float = None
 
     def __init__(self, schema_path: str):
         self.validator = JSONValidator(schema_path)
@@ -21,6 +22,7 @@ class DevelopmentSystemConfigurations:
         self.messaging_system_receiver = data['messaging_system_receiver']
         self.hyperparameters = HyperParameterLimit(data['hyperparameters']['number_of_layers'],
                                                    data['hyperparameters']['number_of_neurons'])
+        self.loss_threshold = data['loss_threshold']
 
     def load_config(self, path: str, should_validate: bool = False):
         with open(path, 'r') as json_file:
