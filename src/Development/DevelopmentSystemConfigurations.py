734fd05204fae6dbf5b5c57b5fcb3d185daa60eb
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-12 19:46:34 +0100

    Add classifiers limit as a configuration parameter

--- src/Development/DevelopmentSystemConfigurations.py
+++ src/Development/DevelopmentSystemConfigurations.py
@@ -12,6 +12,7 @@ class DevelopmentSystemConfigurations:
     loss_threshold: float = None
     port: int = None
     endpoint_url: str = None
+    classifiers_limit: int = None
 
     def __init__(self, schema_path: str):
         self.validator = JSONValidator(schema_path)
@@ -27,6 +28,7 @@ class DevelopmentSystemConfigurations:
         self.loss_threshold = data['loss_threshold']
         self.port = data['port']
         self.endpoint_url = data['endpoint_url']
+        self.classifiers_limit = data['classifiers_limit']
 
     def load_config(self, path: str, should_validate: bool = False):
         with open(path, 'r') as json_file:
