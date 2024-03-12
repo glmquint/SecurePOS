c96773bfbc44c7ef7b6354588c934d5350e6e3b7
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-12 17:00:18 +0100

    Fix wrong path and add treshold to title

--- src/Development/Training/LearningPlotView.py
+++ src/Development/Training/LearningPlotView.py
@@ -8,11 +8,12 @@ class LearningPlotView:
     model: LearningPlotModel = None
 
     def __init__(self, filename: str, learning_plot_model: LearningPlotModel):
-        self.path_to_save = f'src/Development/{filename}.png'
+        self.path_to_save = f'Training/{filename}.png'
         self.model = learning_plot_model
 
     def update(self):
         plt.plot(range(1, self.model.number_of_generations + 1), self.model.loss_curve, label='Loss')
         plt.xlabel('Number of generations')
         plt.ylabel('Loss')
+        plt.title(f'Learning plot (Loss threshold: {self.model.loss_threshold})')
         plt.savefig(self.path_to_save)
