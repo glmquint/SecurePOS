75ef5337c8b2a418e81694af6e400965c15f1429
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-14 12:42:57 +0100

    Change path and name

--- src/Development/Validation/ValidationReportView.py
+++ src/Development/Validation/ValidationReportView.py
@@ -6,9 +6,9 @@ class ValidationReportView:
     path_to_save: str = None
     model: ValidationReportModel = None
 
-    def __init__(self, filename: str, test_report_model: ValidationReportModel):
-        self.path_to_save = f'src/Development/{filename}.csv'
-        self.model = test_report_model
+    def __init__(self, path: str, validation_report_model: ValidationReportModel):
+        self.path_to_save = f'{path}/Validation_Report.csv'
+        self.model = validation_report_model
 
     def update(self):
         df = pd.DataFrame(self.model.to_dict())
