345819d300de4f9e446142171826d81e7571793e
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-14 10:20:51 +0100

    Fix #1

--- src/Development/Training/Scoreboard.py
+++ src/Development/Training/Scoreboard.py
@@ -5,12 +5,16 @@ import bisect
 class Scoreboard:
     classifiers: [Classifier] = None
     mse: [float] = None
-    limit: int = None
     train_error: [float] = None
     validation_error: [float] = None
+    limit: int = None
 
     def __init__(self, limit: int):
         self.limit = limit
+        self.classifiers = []
+        self.mse = []
+        self.train_error = []
+        self.validation_error = []
 
     def insert_classifier(self, classifier: Classifier, mse: float, train_error: float, validation_error: float):
         index = bisect.bisect(self.mse, mse)
@@ -20,5 +24,8 @@ class Scoreboard:
             self.validation_error.insert(index, validation_error)
             classifier.save_model(f"models/{index}.sav")
             self.classifiers.insert(index, classifier)
+            # remove last classifier since the inserted one is better
             self.mse.pop()
             self.classifiers.pop()
+            self.train_error.pop()
+            self.validation_error.pop()
