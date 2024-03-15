5a23177a989cb3e7fdba74c229e799b2385f75dc
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-15 16:33:21 +0100

    Add default and load model from file

--- src/Development/Classifier.py
+++ src/Development/Classifier.py
@@ -13,15 +13,24 @@ class Classifier:
     number_of_layers: int = None
     number_of_iterations: int = None
 
-    def __init__(self, number_of_neurons: int, number_of_layers: int, number_of_iterations: int,
+    def __init__(self, number_of_neurons: int = 0, number_of_layers: int = 0, number_of_iterations: int = 0,
                  name: str = "AverageClassifier"):
         self.number_of_neurons = number_of_neurons
         self.number_of_layers = number_of_layers
         self.number_of_iterations = number_of_iterations
         self.name = name
-        self.model = MLPClassifier(
-            hidden_layer_sizes=tuple(self.number_of_neurons for _ in range(self.number_of_layers)),
-            max_iter=number_of_iterations)
+        if self.number_of_neurons > 0:
+            self.model = MLPClassifier(
+                hidden_layer_sizes=tuple(self.number_of_neurons for _ in range(self.number_of_layers)),
+                max_iter=number_of_iterations)
+
+    def load_model(self, path: str):
+        self.model = joblib.load(f'{path}.sav')
+        self.name = path.split('/')[-1]
+        self.number_of_iterations = self.model.n_iter_
+        self.number_of_layers = self.model.n_layers_
+        self.number_of_neurons = len(self.model.coefs_[1]) # trick to get the number of neurons from a model
+
 
     def save_model(self, path: str):
         # self.bytes_container = BytesIO()
