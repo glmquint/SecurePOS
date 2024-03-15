63cba6a348718f7c01fe9e179c136f632ad96c81
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-15 16:32:03 +0100

    Move TestRow to TestReportModel

--- src/Development/Testing/TestReportModel.py
+++ src/Development/Testing/TestReportModel.py
@@ -1,12 +1,16 @@
-from src.Development.Testing.TestRow import TestRow
-
-
 class TestReportModel:
-    rows: [TestRow] = None
+    classifier_name: str = None
+    validation_error: float = None
+    testing_error: float = None
+    generalization_tolerance: float = None
 
-    def __init__(self, from_tuples: [tuple]):
-        for row in from_tuples:
-            self.rows.append(TestRow(row[0], row[1], row[2], row[3]))
+    def __init__(self, classifier_name: str, validation_error: float, testing_error: float,
+                 generalization_tolerance: float):
+        self.classifier_name = classifier_name
+        self.validation_error = validation_error
+        self.testing_error = testing_error
+        self.generalization_tolerance = generalization_tolerance
 
     def to_dict(self):
-        return [row.to_dict() for row in self.rows]
+        return {'classifier_name': self.classifier_name, 'validation_error': self.validation_error,
+                'testing_error': self.testing_error, 'generalization_tolerance': self.generalization_tolerance}
