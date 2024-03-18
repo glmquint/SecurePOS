d46597e2477d286d8e6f20ed25092126d045b7ce
Fabio <fabizen99@gmail.com>
2024-03-18 15:22:47 +0100

    Refactor ElasticitySample

--- src/DataObjects/ElasticitySample.py
+++ src/DataObjects/ElasticitySample.py
@@ -1,23 +1,22 @@
+import json
 from datetime import datetime
 from time import sleep
 
 
 class ElasticitySample:
-    def __init__(self, number_of_inputs):
-        self.number_of_inputs = number_of_inputs
-        self.timestamps = {}
+    def __init__(self, system, event,timestamp):
+        self.system = system
+        self.event = event
+        self.timestamps = timestamp
 
-    def add_timestamp(self, phase, timestamp):
-        self.timestamps[phase] = timestamp
-
-    def __str__(self):
-        timestamp_str = ", ".join(f"{phase}: {timestamp}" for phase, timestamp in self.timestamps.items())
-        return f"Number of inputs: {self.number_of_inputs}, Timestamps: {timestamp_str}"
+    def to_json(self):
+        return json.dumps({
+            "system": self.system,
+            "event": self.event,
+            "timestamp": self.timestamps.strftime("%Y-%m-%d %H:%M:%S")
+        })
 
 
 if __name__ == "__main__":
-    elasticity_sample = ElasticitySample(5)
-    elasticity_sample.add_timestamp("start", datetime.now())
-    sleep(2)
-    elasticity_sample.add_timestamp("end", datetime.now())
-    print(elasticity_sample)
+    elasticity_sample = ElasticitySample("Production", "Start", datetime.now())
+    print(elasticity_sample.to_json())
