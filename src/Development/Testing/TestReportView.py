ad6af5f11fb79a293c8498e0db764fd028798947
Giacomo Volpi <giacomo.volpi99@gmail.com>
2024-03-18 14:27:03 +0100

    Add delete if existent

--- src/Development/Testing/TestReportView.py
+++ src/Development/Testing/TestReportView.py
@@ -1,3 +1,5 @@
+import os
+
 from src.Development.Testing import TestReportModel
 import pandas as pd
 
@@ -11,5 +13,7 @@ class TestReportView:
         self.model = test_report_model
 
     def update(self):
+        if os.path.exists(self.path_to_save):
+            os.remove(self.path_to_save)
         df = pd.DataFrame(self.model.to_dict(),index=[0])
         df.to_csv(self.path_to_save, index=False)
