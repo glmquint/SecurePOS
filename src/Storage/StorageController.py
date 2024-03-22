1cb8d205897722d41f89b749cbab865b46ea02a7
Guillaume Quint <glmquint@gmail.com>
2024-03-22 08:12:23 +0100

    don't use reserved keyword type in storage controller

--- src/Storage/StorageController.py
+++ src/Storage/StorageController.py
@@ -1,25 +1,26 @@
 from threading import Event
 
 from src.Storage.DBConnector import DBConnector
-from src.DataObjects.PreparedSession import PreparedSession
+from src.DataObjects.Session import PreparedSession
+from src.util import log
 
 
 class StorageController:
     DBConnector = None
-    type = None
+    obj_type = None
 
-    def __init__(self, dbConfig, type):
-        self.type = type
+    def __init__(self, dbConfig, obj_type):
+        self.obj_type = obj_type
         self.DBConnector = DBConnector(name=dbConfig['name'], table_name=dbConfig['table_name'])
         self.count_updated = Event()
         self.count_updated.set()
 
     def save(self, obj):
-        if not issubclass(type(obj), self.type):
-            raise Exception(f'Invalid type, expected {self.type} got {type(obj)}')
+        if not issubclass(type(obj), self.obj_type):
+            raise Exception(f'Invalid type, expected {self.obj_type} got {type(obj)}')
         #row = [obj.to_row()]
         #todo fix this to be parametric
-        row = [self.type.to_row(obj)]
+        row = [self.obj_type.to_row(obj)]
         try:
             self.DBConnector.insert(row)
             self.count_updated.set()
@@ -45,7 +46,7 @@ class StorageController:
     def retrieve_all(self, blocking=True) -> [type]:
         if blocking:
             self.wait_count_updated()
-        return [self.type.from_row(x) for x in self.DBConnector.retrieve()]
+        return [self.obj_type.from_row(x) for x in self.DBConnector.retrieve()]
 
     def count(self, blocking=True) -> int:
         if blocking:
