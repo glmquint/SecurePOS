175a8145daa3ae5dad99579008a362baf87103a7
ertento <ertento@gmail.com>
2024-03-25 16:52:40 +0100

    checked on lan

--- src/Evaluation/EvaluationSystemSender.py
+++ src/Evaluation/EvaluationSystemSender.py
@@ -1,15 +1,22 @@
 import os
 
 from src.JsonIO.JSONSender import JSONSender, JSONValidator
-from src.util import monitorPerformance
+from src.util import monitorPerformance, Message
 
 
 class EvaluationSystemSender:
     def __init__(self):
-        sender = JSONSender(f"{os.path.dirname(__file__)}/../DataObjects/Schema/Label.json", "http://127.0.0.1:6000/messaging_system")
-        return
+        #sender = JSONSender(f"{os.path.dirname(__file__)}/../MessagingSystem/Schema/Label.json", "http://127.0.0.1:6000/messaging_system")
+        #todo fix to be loaded from config
+        self.message_url = "http://192.168.144.104:6000/messaging_system"
+        # TODO: fix schema for message
+        self.messaging_sender = JSONSender(f"{os.path.dirname(__file__)}/../DataObjects/Schema/AttackRiskLabelSchema.json",
+                                           self.message_url)
 
     @monitorPerformance(should_sample_after=True)
-    def sendtomessaging(self,config):
-        self.sender.send({"attackRiskLabel": "low"})
+    def sendtomessaging(self,message):
+        self.messaging_sender.send(message)
         return
+
+#s = EvaluationSystemSender()
+#s.sendtomessaging(Message(msg = "BYPASS DI MERDA"))
